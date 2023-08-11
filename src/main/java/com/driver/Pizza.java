9a00cab6bb7b2a595e42ba6d43df26786190fcfe
ramak <ramak@DESKTOP-8SV8CJ8.bbrouter>
2023-08-11 15:52:17 +0530

    first commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,30 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+    private int cheese = -1;
+    private int toppings = -1;
+    private int takeway = -1;
+    
+    
+  	final int VegPizzaBasePrice = 300;
+  	final int NonvegPizzaBasePrice = 400;
+  	final int ExtraCheesePrice = 80;
+  	final int ExtraToppingsForVegPizza = 70;
+  	final int ExtraToppingsForNonvegPizza = 120;
+  	final int PaperbagPrice = 20;
+  	
+  	
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg==true) {
+        	this.price= VegPizzaBasePrice;
+
+        	}
+        else {
+        	this.price= NonvegPizzaBasePrice;
+        }
+        bill="";
     }
 
     public int getPrice(){
@@ -17,18 +37,61 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+    	if(cheese==-1) {
+    			this.price+=ExtraCheesePrice;
+    			cheese = ExtraCheesePrice;
+    		return;
+    	}
+    	return;
     }
 
     public void addExtraToppings(){
         // your code goes here
+    	if(toppings==-1) {
+    		if(cheese==-1) {
+    			this.price+=ExtraCheesePrice;
+    			cheese = ExtraCheesePrice;
+    	}
+    		 if(isVeg==true) {
+    			 this.price += ExtraToppingsForVegPizza;
+    			 toppings = ExtraToppingsForVegPizza;
+    		 }
+    	       
+    		 else {
+    			 this.price += ExtraToppingsForNonvegPizza;
+    			 toppings = ExtraToppingsForNonvegPizza;
+    		 }
+    			 
+    		 return;
+    		
+    	}
+    	return;
     }
 
     public void addTakeaway(){
         // your code goes here
+    	if(takeway==-1) {
+    		this.price += PaperbagPrice;
+    		takeway = PaperbagPrice;
+    		return;
+    	}
+    	return;
     }
 
     public String getBill(){
         // your code goes here
+    	if(isVeg==true)
+    		bill+="Base Price Of The Pizza: "+VegPizzaBasePrice+"\n";
+    	else
+    		bill+="Base Price Of The Pizza: "+NonvegPizzaBasePrice+"\n";
+    	if(cheese!=-1)
+    		bill+="Extra Cheese Added: "+cheese+"\n";
+    	if(toppings!=-1)
+    		bill+="Extra Toppings Added: "+toppings+"\n";
+    	if(takeway!=-1)
+    		bill+="Paperbag Added: "+takeway+"\n";
+    	bill+="Total Price: "+price;
+    	
         return this.bill;
     }
 }
